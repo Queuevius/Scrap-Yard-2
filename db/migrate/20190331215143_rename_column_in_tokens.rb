93a8161dfb329269c2dca36a08112a13dfea0cf0
maviz <cheema.maviz@gmail.com>
2019-04-01 02:55:40 +0500

    rename migration for heroku

--- db/migrate/20190331215143_rename_column_in_tokens.rb
+++ db/migrate/20190331215143_rename_column_in_tokens.rb
@@ -1,5 +1,5 @@
 class RenameColumnInTokens < ActiveRecord::Migration[5.1]
   def change
-  	change_column :tokens, :type, :token_type
+  	change_column :tokens, :type, :token_type, :string
   end
 end
