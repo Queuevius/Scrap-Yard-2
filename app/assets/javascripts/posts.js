4dacf30deaa22fe28dafde7613646a0bceedb6f1
maviz <cheema.maviz@gmail.com>
2019-06-26 19:31:39 +0500

    basic fileupload on profile..

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -116,6 +116,26 @@ $(".vote").on("click",function(e) {
   });
 
 
+  var readURL = function(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('.profile-pic').attr('src', e.target.result);
+            }
+    
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    
+
+    $(".file-upload").on('change', function(){
+        readURL(this);
+    });
+    
+    $(".upload-button").on('click', function() {
+       $(".file-upload").click();
+    });
 
 
 });
