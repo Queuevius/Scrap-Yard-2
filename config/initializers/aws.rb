3aedeba2fa5bddaf306f1b608066f5a7aaa56c1e
maviz <cheema.maviz@gmail.com>
2019-07-11 06:35:24 +0500

    profile pic uploads and s3 integration

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,6 @@
 Aws.config.update({
   region: 'us-east-1',
-  credentials: Aws::Credentials.new('AKIAJ67BYEXT75QO56KQ', 'kBsuLvQB5qdzxLKemH7WkkkXluWULkBIgA/k3SLg'),
+  credentials: Aws::Credentials.new('XXXX', 'XXXX'),
 })
 
-S3_BUCKET = Aws::S3::Resource.new.bucket("needpediasbucket")
+S3_BUCKET = Aws::S3::Resource.new.bucket("XXXX")
