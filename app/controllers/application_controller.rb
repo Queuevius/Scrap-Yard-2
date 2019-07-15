9a503fab85dbb75d2dc90e1c9cec4b71f8e06584
maviz <cheema.maviz@gmail.com>
2019-07-16 01:49:42 +0500

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -53,6 +53,6 @@ class ApplicationController < ActionController::Base
   protected
   
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+    root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -25,12 +25,13 @@ Rails.application.routes.draw do
       end
     end
   end 
-  root to: 'posts#index'
+
 
   devise_for :users, controllers: {
     # Override the following Devise controllers with our custom versions
     registrations: 'users/registrations'
   }
+
 
   namespace :admin do
     resources :users
