5d1551b0c7eeaed9e5c6dbe0bb2c4c66ded9a3d0
maviz <cheema.maviz@gmail.com>
2019-09-11 04:38:13 +0500

    changes...

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( font-awesome.min.css )
+Rails.application.config.assets.precompile += %w( bootstrap-switch.min.css login-2.min.css plugins.min.css font-awesome.min.css simple-line-icons.min.css bootstrap.min.css uniform.default.css bootstrap-switch.css select2.min.css select2-bootstrap.min.css components-rounded.css )
