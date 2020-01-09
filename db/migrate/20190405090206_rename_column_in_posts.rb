b4d130582faebec78953e21b136be90e158c2264
JanetBanerjee <banerjee.janet@gmail.com>
2020-01-09 11:43:45 -0500

    more layout adjustments

--- db/migrate/20190405090206_rename_column_in_posts.rb
+++ db/migrate/20190405090206_rename_column_in_posts.rb
@@ -2,7 +2,6 @@ class RenameColumnInPosts < ActiveRecord::Migration[5.1]
   def change
   	rename_column :posts, :body, :post_body
   	rename_column :tokens, :body, :token_body
-  	rename_column :comments, :body, :com_body  
   	rename_column :layers, :body, :layer_body  
   
   end
