3cb9005e1b5ddf70dcbcf56148c13d14788f3bde
JanetBanerjee <banerjee.janet@gmail.com>
2020-02-06 14:18:02 -0500

    redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,4 +103,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.serve_static_files = true
+
+  config.middleware.use ChatActionCable
+  config.web_socket_server_url = "wss://need-profile.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://need-profile.herokuapp.com', 'http://need-profile.herokuapp.com']
 end
