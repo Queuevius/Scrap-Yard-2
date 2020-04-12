589a3276c8df872131cab634e54ce799af37ee88
Murtaza khan <murtaza5370@gmail.com>
2020-04-12 11:51:48 +0500

    closed body tag, added jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require rails-ujs
 //= require jquery
-//= require jquery-3.2.1
 // As of Rails 5.1, jquery_ujs was replaced by rails-ujs, which does not
 // depend on jQuery but can be used with applications that use jQuery. This
 // application uses jQuery and we've chosen to use jquery_ujs instead of
